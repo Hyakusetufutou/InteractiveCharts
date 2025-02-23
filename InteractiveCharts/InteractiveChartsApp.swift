2e0cabfe4ecd4533baee705a3ed18abee8d2edd6
Hyakusetufutou <Hyakusetufutou358@gmail.com>
2025-02-23 10:53:39 +0900

    Initial Commit

---
+++ InteractiveCharts/InteractiveChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveChartsApp.swift
+//  InteractiveCharts
+//  
+//  
+//
+
+import SwiftUI
+
+@main
+struct InteractiveChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
